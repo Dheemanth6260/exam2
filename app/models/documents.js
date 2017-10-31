260f81b359df94034f5e50bee609cc1d605bf0a6
Dheemanth6260 <33210281+Dheemanth6260@users.noreply.github.com>
2017-10-30 19:35:54 -0500

    Update documents.js

--- app/models/documents.js
+++ app/models/documents.js
@@ -4,14 +4,10 @@ var Schema = Mongoose.Schema;
  
 var documentSchema = new Schema({
  
-    firstName: { type: String, required: true }, // first name required
-    lastName: { type: String, required: true }, // last name required 
-    Status: {type: Boolean, default: true}, // status with default true
-    email: {type: String, required: true, unique: true},
+  
     char: { type: String, required: true }, 
-    idnumber: { type: Number, min: 16, max: 60}, 
-    password: { type: String, required: true },
-    dateRegistered: {type: Date, default: Date.now },
+    number: { type: Number, min: 16, max: 60}, 
+   
  
     
       
